8f478d6169c7aa1abcd2e71f8d7dc568797b6fff
alsotang <alsotang@gmail.com>
2017-03-18 17:22:08 +0800

    logger的位置

--- common/logger.js
+++ common/logger.js
@@ -7,7 +7,7 @@ var log4js = require('log4js');
 log4js.configure({
   appenders: [
     { type: 'console' },
-    { type: 'file', filename: 'logs/cheese.log', category: 'cheese' }
+    { type: 'file', filename: '/data/nodeclub_log/cheese.log', category: 'cheese' }
   ]
 });
 
